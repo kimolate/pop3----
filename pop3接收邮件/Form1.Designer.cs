dbd0768c0d3475b1f0874e7dbed515d70596687f
kimolate <kimolatelei@outlook.com>
2015-09-15 18:59:23 +0800

    V0.1
    实现接收邮件，但不能解码中文邮件

---
+++ pop3接收邮件/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace pop3接收邮件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Connect = new System.Windows.Forms.Button();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MailNum = new System.Windows.Forms.TextBox();
+            this.BackupChBox = new System.Windows.Forms.CheckBox();
+            this.Retrieve = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PopServer = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Status = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "POP3服务器地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "用户名：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "密码：";
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(340, 12);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 3;
+            this.Connect.Text = "连接";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.Location = new System.Drawing.Point(340, 45);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.Disconnect.TabIndex = 4;
+            this.Disconnect.Text = "断开连接";
+            this.Disconnect.UseVisualStyleBackColor = true;
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "信息：";
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(19, 122);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(396, 181);
+            this.Message.TabIndex = 6;
+            this.Message.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "邮件编号：";
+            // 
+            // MailNum
+            // 
+            this.MailNum.Location = new System.Drawing.Point(90, 309);
+            this.MailNum.Name = "MailNum";
+            this.MailNum.Size = new System.Drawing.Size(100, 21);
+            this.MailNum.TabIndex = 8;
+            // 
+            // BackupChBox
+            // 
+            this.BackupChBox.AutoSize = true;
+            this.BackupChBox.Location = new System.Drawing.Point(197, 310);
+            this.BackupChBox.Name = "BackupChBox";
+            this.BackupChBox.Size = new System.Drawing.Size(156, 16);
+            this.BackupChBox.TabIndex = 9;
+            this.BackupChBox.Text = "在邮件服务器上保留备份";
+            this.BackupChBox.UseVisualStyleBackColor = true;
+            // 
+            // Retrieve
+            // 
+            this.Retrieve.Location = new System.Drawing.Point(360, 309);
+            this.Retrieve.Name = "Retrieve";
+            this.Retrieve.Size = new System.Drawing.Size(75, 23);
+            this.Retrieve.TabIndex = 10;
+            this.Retrieve.Text = "收取邮件";
+            this.Retrieve.UseVisualStyleBackColor = true;
+            this.Retrieve.Click += new System.EventHandler(this.Retrieve_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 333);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "状态：";
+            // 
+            // PopServer
+            // 
+            this.PopServer.Location = new System.Drawing.Point(121, 13);
+            this.PopServer.Name = "PopServer";
+            this.PopServer.Size = new System.Drawing.Size(213, 21);
+            this.PopServer.TabIndex = 13;
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(121, 41);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(213, 21);
+            this.Username.TabIndex = 14;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(121, 76);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(213, 21);
+            this.Password.TabIndex = 15;
+            // 
+            // Status
+            // 
+            this.Status.FormattingEnabled = true;
+            this.Status.ItemHeight = 12;
+            this.Status.Location = new System.Drawing.Point(21, 349);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(403, 76);
+            this.Status.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 426);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.PopServer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Retrieve);
+            this.Controls.Add(this.BackupChBox);
+            this.Controls.Add(this.MailNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "POP3邮件接收程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Disconnect;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox Message;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MailNum;
+        private System.Windows.Forms.CheckBox BackupChBox;
+        private System.Windows.Forms.Button Retrieve;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PopServer;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.ListBox Status;
+
+    }
+}
+
---
+++ pop3接收邮件/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace pop3接收邮件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public TcpClient Server;
+        public NetworkStream NetStrm;
+        public StreamReader RdStrm;
+        public string Data;
+        public byte[] szData;
+        public string CRLF = "\r\n";
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            //将光标置为等待状态
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            //用110端口新建POP3服务器连接
+            Server = new TcpClient(PopServer.Text, 110);
+            Status.Items.Clear();
+
+            try
+            {
+                //初始化
+                NetStrm = Server.GetStream();
+                RdStrm = new StreamReader(Server.GetStream());
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //登录服务器过程
+                Data = "USER  " + Username.Text + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                Data = "PASS " + Password.Text + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //向服务器发送STAT命令，从而取得邮箱的相关信息：邮件数量和大小
+                Data = "STAT" + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                Status.Items.Add(RdStrm.ReadLine());
+
+                //改变按钮的状态
+                Connect.Enabled = false;
+                Disconnect.Enabled = true;
+                Retrieve.Enabled = true;
+
+                //将光标置回原来的状态
+                Cursor.Current = cr;
+
+            }
+            catch (InvalidOperationException err)
+            {
+                Status.Items.Add("Error: " + err.ToString());
+            }
+
+        }
+
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            //将光标置为等待状态
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            //向服务器发送QUIT命令从而结束和POP3服务器的会话
+            Data = "QUIT" + CRLF;
+            szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+            NetStrm.Write(szData, 0, szData.Length);
+            Status.Items.Add(RdStrm.ReadLine());
+
+            //断开连接
+            NetStrm.Close();
+            RdStrm.Close();
+
+            //改变按钮的状态
+            Connect.Enabled = true;
+            Disconnect.Enabled = false;
+            Retrieve.Enabled = false;
+
+            //将光标置回原来的状态
+            Cursor.Current = cr;
+
+        }
+
+        private void Retrieve_Click(object sender, EventArgs e)
+        {
+            //将光标置为等待状态
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+            string szTemp;
+            Message.Clear();
+
+            try
+            {
+                //根据邮件编号从服务器获得相应邮件
+                Data = "RETR " + MailNum.Text + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                NetStrm.Write(szData, 0, szData.Length);
+                szTemp = RdStrm.ReadLine();
+
+                if (szTemp[0] != '-')
+                {
+                    //不断地读取邮件内容，只到结束标志：英文句号
+                    while (szTemp != ".")
+                    {
+                        Message.Text += szTemp;
+                        szTemp = RdStrm.ReadLine();
+                    }
+
+                    //若BackupChBox未选中，则收取邮件后，删除保留在服务器上的邮件
+                    if (BackupChBox.Checked == false)
+                    {
+                        Data = "DELE" + MailNum.Text + CRLF;
+                        szData = System.Text.Encoding.ASCII.GetBytes(Data.ToCharArray());
+                        NetStrm.Write(szData, 0, szData.Length);
+                        Status.Items.Add(RdStrm.ReadLine());
+                    }
+                }
+                else
+                {
+                    Status.Items.Add(szTemp);
+                }
+
+                //将光标置回原来的状态
+                Cursor.Current = cr;
+            }
+
+            catch (InvalidOperationException err)
+            {
+                Status.Items.Add("Error: " + err.ToString());
+            }
+
+        }
+
+    }
+}
